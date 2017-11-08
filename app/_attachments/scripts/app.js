e03b351c3edaac9b54c29e4b43c9c48b855faf41
wa1id <wyachou95@gmail.com>
2017-11-08 15:48:56 +0100

    alle movies opslaan in couchdb onder acteurnaam

--- app/_attachments/scripts/app.js
+++ app/_attachments/scripts/app.js
@@ -13,14 +13,24 @@ angular.module('moviesApp', ['ngRoute'])
           });
 })
 
-.controller('homeCtrl', function($scope, acteurSrv) {
+.controller('homeCtrl', function($scope, acteurSrv, putDocSrv) {
 	
 	$('#searchButton').on('click', function(e) {
 		
 		var acteur = $('#acteurText').val();
 		
 		acteurSrv.getActeur(acteur).then(function(data) {
-			console.log(data);
+			//console.log(data);
+			var doc = {};
+			var movies = data.data[0].filmography.actor;
+			doc.acteur = acteur;
+			doc.movies = movies;
+
+			putDocSrv.putDoc(acteur,doc).then(function(response){
+	            
+	        },function(response){
+	            
+	        })
 			
 		});
 	});
@@ -42,3 +52,24 @@ angular.module('moviesApp', ['ngRoute'])
 		return q.promise;
 	}
 })
+
+.factory('putDocSrv',['$http','$q',function($http,$q){
+    return {
+    	putDoc: function(key, doc){
+            var method = 'PUT';
+            var q = $q.defer();
+            $http({
+                url: '../../' + key,
+                method: method,
+                data: JSON.stringify(doc),
+                contentType: "application/json"
+            }).success(function (data, status, headers, config) {
+                q.resolve(method + " is goed uitgevoerd: Status " + status);
+             })
+                .error(function (data, status, headers, config) {
+                 q.reject(method + " is niet goed uitgevoerd: Status " + status);
+             });
+            return q.promise;
+        }
+    }
+}]);
